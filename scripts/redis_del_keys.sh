b5dac3dd0d8ebf3ee17dbe3fae639b5fc10a9748
Edoardo Rossi <edd.rossi@gmail.com>
2015-07-07 14:01:49 +0200

    Clean my redis, please...

--- scripts/redis_del_keys.sh
+++ scripts/redis_del_keys.sh
@@ -1,3 +1,4 @@
 redis-cli keys "lock_*" | grep "lock_[a-z0-9]\+$" | xargs redis-cli DEL
 redis-cli keys "polip*" | grep "polip" | xargs redis-cli DEL 
 redis-cli keys "backe*" | grep "backe" | xargs redis-cli DEL 
+redis-cli keys "gsp*" | grep "gsp" | xargs redis-cli DEL
