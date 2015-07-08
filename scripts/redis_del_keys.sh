a011478677f24ce0c0278eaf432bd0ee1384b198
Edoardo Rossi <edd.rossi@gmail.com>
2015-07-08 12:33:26 +0200

    Cleanup my Redis...

--- scripts/redis_del_keys.sh
+++ scripts/redis_del_keys.sh
@@ -1,4 +1,5 @@
-redis-cli keys "lock_*" | grep "lock_[a-z0-9]\+$" | xargs redis-cli DEL
-redis-cli keys "polip*" | grep "polip" | xargs redis-cli DEL 
-redis-cli keys "backe*" | grep "backe" | xargs redis-cli DEL 
-redis-cli keys "gsp*" | grep "gsp" | xargs redis-cli DEL
+echo "redis: DEL bp_* ..." ; redis-cli keys "bp_*" | grep "bp" | xargs redis-cli DEL
+echo "redis: DEL lock_* ..." ; redis-cli keys "lock_*" | grep "lock_[a-z0-9]\+$" | xargs redis-cli DEL
+echo "redis: DEL polip* ..." ; redis-cli keys "polip*" | grep "polip" | xargs redis-cli DEL
+echo "redis: DEL backe* ..." ; redis-cli keys "backe*" | grep "backe" | xargs redis-cli DEL
+echo "redis: DEL gsp* ..." ; redis-cli keys "gsp*" | grep "gsp" | xargs redis-cli DEL
